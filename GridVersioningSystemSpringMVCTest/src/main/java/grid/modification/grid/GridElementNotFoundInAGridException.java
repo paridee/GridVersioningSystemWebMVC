d98ffeef3b9a3ede93c79c8b6bd87296d082d2b7
lollo <lollo@lollo-K53SD>
2016-02-17 15:52:32 +0100

    GEResolution fix

---
+++ GridVersioningSystemSpringMVCTest/src/main/java/grid/modification/grid/GridElementNotFoundInAGridException.java
@@ -0,0 +1,7 @@
+package grid.modification.grid;
+
+public class GridElementNotFoundInAGridException extends Exception {
+	public GridElementNotFoundInAGridException(String msg){
+		super(msg);
+	}
+}
