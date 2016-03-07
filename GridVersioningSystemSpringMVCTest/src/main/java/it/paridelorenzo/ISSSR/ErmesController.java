7e927cc43fa1dbf4cd9edb4bb38683baedf6b50a
lollo <lollo@lollo-VirtualBox>
2016-03-07 17:19:55 +0100

    Json Controller update

--- GridVersioningSystemSpringMVCTest/src/main/java/it/paridelorenzo/ISSSR/ErmesController.java
+++ GridVersioningSystemSpringMVCTest/src/main/java/it/paridelorenzo/ISSSR/ErmesController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import grid.JSONFactory;
 import grid.JSONFactory.JSONType;
 import grid.entities.Grid;
+import grid.entities.GridElement;
+import grid.entities.Project;
 import grid.interfaces.services.GridElementService;
 import grid.interfaces.services.GridService;
 import grid.interfaces.services.ProjectService;
@@ -56,16 +58,46 @@ public class ErmesController {
 				+ request.getObject() + " from project " + request.getProject());
 		Persistence persistence = new Persistence();
 		String tmp = "";
+		Project currentPrj=this.projectService.getProjectByProjectId(request.getProject());
+		int projid=currentPrj.getId();
 		if (request.getObject().equals("Project-info")) {  //TODO Deve andare su fase 6 (ora è simulata)
 			logger.info("PROJECT INFO");
 			tmp = persistence.obtainProject();
 		} else if (request.getObject().equals("LatestGrid")) {
-			/*int projid=this.projectService.getProjectByProjectId(request.getProject()).getId();
 			Grid latest=this.gridService.getLatestWorkingGrid(projid);
-			tmp = this.jsonFactory.obtainJson(latest,JSONType.FIRST , null).toString();*/
-			tmp = persistence.obtainGrid();
+			tmp = this.jsonFactory.obtainJson(latest,JSONType.FIRST , null).toString();
 			logger.info(tmp);
 		}
+		else if (request.getObject().equals("GridHistory")) {	//array con id delle grid working
+			tmp = this.gridService.getJsonWorkingGridLog(currentPrj);
+			logger.info(tmp);
+		}
+		else if (request.getObject().equals("Grid")) {	
+			int gid=Integer.parseInt(request.getData());
+			tmp =this.jsonFactory.obtainJson(this.gridService.getGridById(gid),JSONType.FIRST , null,true).toString();
+			logger.info(tmp);
+		}
+		else if (request.getObject().equals("GridElement")) {	
+			String command=request.getData();
+			String[] items=command.split(",");
+			int id=Integer.parseInt(items[0]);
+			String type=items[1];
+			GridElement current=this.gridElementService.getElementById(id, type);
+			tmp =this.jsonFactory.obtainJson(current, JSONType.FIRST).toString();
+			logger.info(tmp);
+		}
+		else if (request.getObject().equals("GridElementHistory")) {	
+			String command=request.getData();
+			String[] items=command.split(",");
+			String label=items[0];
+			String type=items[1];
+			tmp=this.gridElementService.getJsonWorkingLogList(label, type);
+			logger.info(tmp);
+		}
+		
+		
+		
+		
 		else{
 			logger.info(request.getObject());
 		}
